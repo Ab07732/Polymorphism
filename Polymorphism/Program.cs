43620e68fc6efb8d969ba46d06885abcf3eaf107
Austin Branham <austinhb1993@gmail.com>
2023-03-28 14:05:27 -0600

    Add project files.

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tutlane
+{
+    // Base Class
+    public class BClass
+    {
+        public virtual void GetInfo()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+    // Derived Class
+    public class DClass : BClass
+    {
+        public override void GetInfo()
+        {
+            Console.WriteLine("Welcome to Mars");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DClass d = new DClass();
+            d.GetInfo();
+            BClass b = new BClass();
+            b.GetInfo();
+            Console.WriteLine("\nPress Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}
